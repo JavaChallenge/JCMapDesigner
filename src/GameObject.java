2b3142500c5533d00d3af934d85c54098984e55a
Hadi Khodabande <h74629@gmail.com>
2015-02-01 10:28:10 +0330

    Game objects added.

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,9 +1,12 @@
 import com.google.gson.JsonArray;
 
 import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
 
-public class Cell {
+/**
+ * Created by Hadi on 2/1/2015 8:29 AM.
+ */
+public class GameObject {
     public static ArrayList<String> sKeys = new ArrayList<>();
     public static ArrayList<Number> sDefault = new ArrayList<>();
 
@@ -13,30 +16,35 @@ public class Cell {
         return tmp.toString();
     }
 
-
     int color[] = new int[] {255, 255, 255};
     public ArrayList<Number> mValues = new ArrayList<>();
-    public GameObject object;
+    public int i, j;
 
-    public Cell() {
+    public GameObject() {
+        mValues.addAll(sDefault);
     }
 
-    public Cell(JsonArray values) {
+    public GameObject(JsonArray values) {
+        i = values.get(0).getAsInt();
+        j = values.get(1).getAsInt();
         for (int i = 0; i < sKeys.size(); i++)
-            mValues.add(values.get(i).getAsNumber());
+            mValues.add(values.get(i+2).getAsNumber());
     }
 
     @Override
     public String toString() {
-        return mValues.toString();
+        ArrayList<Number> tmp = new ArrayList<>();
+        tmp.add(i);
+        tmp.add(j);
+        tmp.addAll(mValues);
+        return tmp.toString();
     }
 
     public void paint(Graphics g, int size) {
         g.setColor(new Color(color[0], color[1], color[2]));
-        g.fillRect(0, 0, size, size);
+        g.fillArc((int)(size*0.1), (int)(size*0.1), (int)(size*0.8), (int)(size*0.8), 0, 360);
         g.setColor(Color.BLACK);
-        g.drawRect(0, 0, size, size);
-        if (object != null)
-            object.paint(g, size);
+        g.drawArc((int)(size*0.1), (int)(size*0.1), (int)(size * 0.8), (int)(size*0.8), 0, 360);
     }
+
 }
--- src/MapDesigner.java
+++ src/MapDesigner.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import javax.script.ScriptEngine;
@@ -25,13 +26,19 @@ public class MapDesigner {
 	public static JFrame frame;
 	public static MapPanel map;
 	public static StatusBar status;
-	public static JButton selectButton, changeButton;
+	public static JPanel fieldsPanel;
+	public static JPanel toolsPanel;
+	public static JButton selectButton, changeButton, cellButton, objectButton, defButton, remObjects;
+	public static ArrayList<PropertyField> cellFields = new ArrayList<>();
+	public static ArrayList<PropertyField> objectFields = new ArrayList<>();
+
 	public static int w, h;
 	public static Cell[][] cells;
 	public static int clickMode = 0;
-	public static ArrayList<PropertyField> fields = new ArrayList<>();
-	public static JPanel fieldsPanel;
-	public static String colorExpr[];
+	public static boolean cellMode;
+	public static boolean removeMode;
+	public static String cellColor[];
+	public static String objectColor[];
 	public static ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
 
 	public static void main(String[] args) {
@@ -41,22 +48,26 @@ public class MapDesigner {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		JPanel toolsPanel = new JPanel();
+		toolsPanel = new JPanel();
 		toolsPanel.setLayout(new BorderLayout());
-		toolsPanel.setPreferredSize(new Dimension(200, 0));
+		toolsPanel.setPreferredSize(new Dimension(225, 0));
 		toolsPanel.setBackground(new Color(225, 225, 225));
 		frame.add(toolsPanel, BorderLayout.EAST);
 		frame.add(map = new MapPanel(), BorderLayout.CENTER);
 		frame.add(status = new StatusBar(), BorderLayout.SOUTH);
 
 		JPanel toolsButtons = new JPanel();
-		toolsButtons.setLayout(new GridLayout(1, 0));
+		toolsButtons.setLayout(new GridLayout(3, 2));
 		toolsPanel.add(toolsButtons, BorderLayout.SOUTH);
+
+		fieldsPanel = new JPanel();
+		fieldsPanel.setLayout(new GridLayout(0, 1));
+		toolsPanel.add(fieldsPanel, BorderLayout.NORTH);
 
 		selectButton = new JButton("Pick");
 		selectButton.addActionListener(e -> {
 			clickMode = clickMode == 1 ? 0 : 1;
-			updateBtnColors();
+			updateMode();
 			status.repaintme();
 		});
 		toolsButtons.add(selectButton);
@@ -64,16 +75,39 @@ public class MapDesigner {
 		changeButton = new JButton("Clone");
 		changeButton.addActionListener(e -> {
 			clickMode = clickMode == 2 ? 0 : 2;
-			updateBtnColors();
+			updateMode();
 			status.repaintme();
 		});
 		toolsButtons.add(changeButton);
 
-		updateBtnColors();
+		cellButton = new JButton("Edit Cells");
+		cellButton.addActionListener(e -> {
+			cellMode = true;
+			updateMode();
+		});
+		toolsButtons.add(cellButton);
 
-		fieldsPanel = new JPanel();
-		fieldsPanel.setLayout(new GridLayout(0, 1));
-		toolsPanel.add(fieldsPanel, BorderLayout.NORTH);
+		objectButton = new JButton("Edit Objects");
+		objectButton.setBackground(Color.lightGray);
+		objectButton.addActionListener(e -> {
+			cellMode = false;
+			updateMode();
+		});
+		toolsButtons.add(objectButton);
+
+		defButton = new JButton("Load Defaults");
+		defButton.setBackground(Color.lightGray);
+		defButton.addActionListener(e -> loadDefaults());
+		toolsButtons.add(defButton);
+
+		remObjects = new JButton("Remove");
+		remObjects.addActionListener(e -> {
+			removeMode = !removeMode;
+			updateMode();
+		});
+		toolsButtons.add(remObjects);
+
+		updateMode();
 
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(new Color(235, 235, 235));
@@ -95,9 +129,12 @@ public class MapDesigner {
 		JMenuItem addProperty = new JMenuItem("Add Property");
 		addProperty.addActionListener(e -> addProperty());
 		optionsMenu.add(addProperty);
-		JMenuItem coloring = new JMenuItem("Define Coloring");
-		coloring.addActionListener(e -> defineColoring());
-		optionsMenu.add(coloring);
+		JMenuItem cellColoring = new JMenuItem("Cell Coloring");
+		cellColoring.addActionListener(e -> defineCellColoring());
+		optionsMenu.add(cellColoring);
+		JMenuItem objectColoring = new JMenuItem("Object Coloring");
+		objectColoring.addActionListener(e -> defineObjectColoring());
+		optionsMenu.add(objectColoring);
 
 		menuBar.setVisible(true);
 		frame.setJMenuBar(menuBar);
@@ -105,6 +142,15 @@ public class MapDesigner {
 		frame.setVisible(true);
 
 		newMap(10, 10);
+	}
+
+	private static void loadDefaults() {
+		if (cellMode)
+			for (int i = 0; i < cellFields.size(); i++)
+				cellFields.get(i).field.setText(Cell.sDefault.get(i).toString());
+		else
+			for (int i = 0; i < objectFields.size(); i++)
+				objectFields.get(i).field.setText(GameObject.sDefault.get(i).toString());
 	}
 
 	public static void newMap() {
@@ -125,14 +171,21 @@ public class MapDesigner {
 		h = height;
 		cells = new Cell[h][w];
 		Cell.sKeys.clear();
-		fields.clear();
-		colorExpr = new String[] {"1", "1", "1"};
+		cellFields.clear();
+		objectFields.clear();
+		cellColor = new String[] {"1", "1", "1"};
+		objectColor = new String[] {"1", "0.8", "0.8"};
 		for (int i = 0; i < h; i++)
 			for (int j = 0; j < w; j++)
 				cells[i][j] = new Cell();
 		map.reset();
+		cellMode = true;
+		removeMode = false;
+		clickMode = 0;
+		updateMode();
 		repaintFields();
-		updateColors();
+		updateCellColors();
+		updateObjectColors();
 	}
 
 	public static void saveMap() {
@@ -164,22 +217,40 @@ public class MapDesigner {
 				w = root.get("width").getAsJsonPrimitive().getAsInt();
 				h = root.get("height").getAsJsonPrimitive().getAsInt();
 				cells = new Cell[h][w];
+				cellMode = true;
+				removeMode = false;
+				clickMode = 0;
+				updateMode();
 				Cell.sKeys.clear();
-				fields.clear();
-				root.get("keys").getAsJsonArray().forEach(je -> Cell.sKeys.add(je.getAsString()));
-				root.get("default").getAsJsonArray().forEach(je -> Cell.sDefault.add(je.getAsNumber()));
-				colorExpr = root.get("coloring").getAsString().split(",");
+				Cell.sDefault.clear();
+				GameObject.sKeys.clear();
+				GameObject.sDefault.clear();
+				cellFields.clear();
+				objectFields.clear();
+				root.get("cellKeys").getAsJsonArray().forEach(je -> Cell.sKeys.add(je.getAsString()));
+				root.get("cellDefaults").getAsJsonArray().forEach(je -> Cell.sDefault.add(je.getAsNumber()));
+				root.get("objectKeys").getAsJsonArray().forEach(je -> GameObject.sKeys.add(je.getAsString()));
+				root.get("objectDefaults").getAsJsonArray().forEach(je -> GameObject.sDefault.add(je.getAsNumber()));
+				cellColor = root.get("cellColoring").getAsString().split(",");
+				objectColor = root.get("objectColoring").getAsString().split(",");
 				JsonArray rows = root.get("cells").getAsJsonArray();
 				for (int i = 0; i < h; i++) {
 					JsonArray row = rows.get(i).getAsJsonArray();
 					for (int j = 0; j < w; j++)
 						cells[i][j] = new Cell(row.get(j).getAsJsonArray());
 				}
+				JsonArray objs = root.getAsJsonArray("objects").getAsJsonArray();
+				for (int i = 0; i < objs.size(); i++) {
+					GameObject obj = new GameObject(objs.get(i).getAsJsonArray());
+					cells[obj.i][obj.j].object = obj;
+				}
 				for (int i = 0; i < Cell.sKeys.size(); i++)
-					fields.add(new PropertyField(Cell.sKeys.get(i), Cell.sDefault.get(i)));
+					cellFields.add(new PropertyField(Cell.sKeys.get(i), Cell.sDefault.get(i)));
+				for (int i = 0; i < GameObject.sKeys.size(); i++)
+					objectFields.add(new PropertyField(GameObject.sKeys.get(i), GameObject.sDefault.get(i)));
 				map.reset();
 				repaintFields();
-				updateColors();
+				updateCellColors();
 			} catch (IOException e) {
 				JOptionPane.showMessageDialog(frame, e);
 			}
@@ -191,12 +262,22 @@ public class MapDesigner {
 				.append("{\n")
 				.append("\t\"width\": ").append(w).append(",\n")
 				.append("\t\"height\": ").append(h).append(",\n")
-				.append("\t\"keys\": ").append(Cell.properties()).append(",\n")
-				.append("\t\"default\": ").append(Cell.sDefault).append(",\n")
-				.append("\t\"coloring\": ").append('"').append(colorExpr[0]).append(',').append(colorExpr[1]).append(',').append(colorExpr[2]).append('"').append(",\n")
+				.append("\t\"cellKeys\": ").append(Cell.properties()).append(",\n")
+				.append("\t\"cellDefaults\": ").append(Cell.sDefault).append(",\n")
+				.append("\t\"cellColoring\": ").append('"').append(cellColor[0]).append(',').append(cellColor[1]).append(',').append(cellColor[2]).append('"').append(",\n")
 				.append("\t\"cells\": ").append(Arrays.deepToString(cells)).append("\n")
+				.append("\t\"objectKeys\": ").append(GameObject.properties()).append(",\n")
+				.append("\t\"objectDefaults\": ").append(GameObject.sDefault).append(",\n")
+				.append("\t\"objectColoring\": ").append('"').append(objectColor[0]).append(',').append(objectColor[1]).append(',').append(objectColor[2]).append('"').append(",\n")
+				.append("\t\"objects\": ").append(objectsToString()).append("\n")
 				.append("}")
 				.toString();
+	}
+
+	public static String objectsToString() {
+		ArrayList<GameObject> tmp = new ArrayList<>();
+		foreachGameObject(tmp::add);
+		return tmp.toString();
 	}
 
 	public static void addProperty() {
@@ -211,10 +292,17 @@ public class MapDesigner {
 			value = parseNumber(number);
 		}
 		final Number finalValue = value;
-		Cell.sKeys.add(name);
-		Cell.sDefault.add(value);
-		foreachCell(c -> c.mValues.add(finalValue));
-		fields.add(new PropertyField(name, finalValue));
+		if (cellMode) {
+			Cell.sKeys.add(name);
+			Cell.sDefault.add(value);
+			foreachCell(c -> c.mValues.add(finalValue));
+			cellFields.add(new PropertyField(name, finalValue));
+		} else {
+			GameObject.sKeys.add(name);
+			GameObject.sDefault.add(value);
+			foreachGameObject(o -> o.mValues.add(finalValue));
+			objectFields.add(new PropertyField(name, finalValue));
+		}
 		repaintFields();
 	}
 
@@ -238,61 +326,124 @@ public class MapDesigner {
 				action.accept(cells[i][j]);
 	}
 
+	public static void foreachGameObject(Consumer<GameObject> action) {
+		foreachCell(c -> {
+			if (c.object != null)
+				action.accept(c.object);
+		});
+	}
+
 	public static void repaintFields() {
 		fieldsPanel.removeAll();
-		fields.forEach(fieldsPanel::add);
+		if (cellMode)
+			cellFields.forEach(fieldsPanel::add);
+		else
+			objectFields.forEach(fieldsPanel::add);
 		fieldsPanel.revalidate();
 	}
 
 	public static void onClick(Point p) {
 		if (p.x < 0 || p.x > w || p.y < 0 || p.y > h)
 			return;
-		if (clickMode == 1) { // pick
-			Cell src = cells[p.y][p.x];
-			for (int i = 0; i < fields.size(); i++)
-				fields.get(i).field.setText(src.mValues.get(i).toString());
-			fieldsPanel.revalidate();
-		} else if (clickMode == 2) { // clone
-			Cell dst = cells[p.y][p.x];
-			for (int i = 0; i < fields.size(); i++) {
-				Number num = parseNumber(fields.get(i).field.getText());
-				if (num != null)
-					dst.mValues.set(i, num);
-			}
-			updateColor(p.y, p.x);
+		if (removeMode) {
+			if (cellMode)
+				return;
+			cells[p.y][p.x].object = null;
 			map.repaint();
+			return;
+		}
+		if (cellMode) {
+			if (clickMode == 1) { // pick
+				Cell src = cells[p.y][p.x];
+				for (int i = 0; i < cellFields.size(); i++)
+					cellFields.get(i).field.setText(src.mValues.get(i).toString());
+				fieldsPanel.revalidate();
+			} else if (clickMode == 2) { // clone
+				Cell dst = cells[p.y][p.x];
+				for (int i = 0; i < cellFields.size(); i++) {
+					Number num = parseNumber(cellFields.get(i).field.getText());
+					if (num != null)
+						dst.mValues.set(i, num);
+				}
+				updateCellColor(p.y, p.x);
+				map.repaint();
+			}
+		} else {
+			if (clickMode == 1) { // pick
+				GameObject src = cells[p.y][p.x].object;
+				if (src == null)
+					return;
+				for (int i = 0; i < objectFields.size(); i++)
+					objectFields.get(i).field.setText(src.mValues.get(i).toString());
+				fieldsPanel.revalidate();
+			} else if (clickMode == 2) { // clone
+				GameObject dst = new GameObject();
+				Cell cell = cells[p.y][p.x];
+				dst.i = p.y;
+				dst.j = p.x;
+				cell.object = dst;
+				for (int i = 0; i < objectFields.size(); i++) {
+					Number num = parseNumber(objectFields.get(i).field.getText());
+					if (num != null)
+						dst.mValues.set(i, num);
+				}
+				updateObjectColor(dst);
+				map.repaint();
+			}
 		}
 	}
 
-	public static void defineColoring() {
-		String expr = JOptionPane.showInputDialog(frame, "Enter coloring formula.\nValues of properties are given by p1, p2, etc.\nColoring formula must be in the from R,G,B where each one is between 0 and 1.", colorExpr[0] + "," + colorExpr[1] + "," + colorExpr[2]);
+	public static void defineCellColoring() {
+		String expr = JOptionPane.showInputDialog(frame, "Enter coloring formula.\nValues of properties are given by p1, p2, etc.\nColoring formula must be in the from R,G,B where each one is between 0 and 1.", cellColor[0] + "," + cellColor[1] + "," + cellColor[2]);
 		if (expr == null)
 			return;
 		String rgb[] = expr.split(",");
 		if (rgb.length != 3)
 			return;
-		colorExpr = rgb;
-		updateColors();
+		cellColor = rgb;
+		updateCellColors();
 	}
 
-	public static void updateColors() {
+	public static void defineObjectColoring() {
+		String expr = JOptionPane.showInputDialog(frame, "Enter coloring formula.\nValues of properties are given by p1, p2, etc.\nColoring formula must be in the from R,G,B where each one is between 0 and 1.", objectColor[0] + "," + objectColor[1] + "," + objectColor[2]);
+		if (expr == null)
+			return;
+		String rgb[] = expr.split(",");
+		if (rgb.length != 3)
+			return;
+		objectColor = rgb;
+		updateObjectColors();
+	}
+
+	public static void updateCellColors() {
 		boolean s = true;
 		for (int i = 0; i < h; i++)
 			for (int j = 0; j < w; j++)
-				s &= updateColor(i, j);
+				s &= updateCellColor(i, j);
 		if (!s)
 			JOptionPane.showMessageDialog(frame, "Error in color evaluation.");
 		map.repaint();
 	}
 
-	public static boolean updateColor(int i, int j) {
+	public static void updateObjectColors() {
+		boolean s = true;
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+				if (cells[i][j].object != null)
+					s &= updateObjectColor(cells[i][j].object);
+		if (!s)
+			JOptionPane.showMessageDialog(frame, "Error in color evaluation.");
+		map.repaint();
+	}
+
+	public static boolean updateCellColor(int i, int j) {
 		int size = cells[i][j].mValues.size();
 		for (int k = 0; k < size; k++)
 			engine.put("p"+(k+1), cells[i][j].mValues.get(k));
 		try {
-			Number r = (Number) engine.eval(colorExpr[0]);
-			Number g = (Number) engine.eval(colorExpr[1]);
-			Number b = (Number) engine.eval(colorExpr[2]);
+			Number r = (Number) engine.eval(cellColor[0]);
+			Number g = (Number) engine.eval(cellColor[1]);
+			Number b = (Number) engine.eval(cellColor[2]);
 			cells[i][j].color[0] = (int) Math.min(Math.max(256 * r.doubleValue(), 0), 255);
 			cells[i][j].color[1] = (int) Math.min(Math.max(256 * g.doubleValue(), 0), 255);
 			cells[i][j].color[2] = (int) Math.min(Math.max(256 * b.doubleValue(), 0), 255);
@@ -303,7 +454,34 @@ public class MapDesigner {
 		return true;
 	}
 
-	public static void updateBtnColors() {
+	public static boolean updateObjectColor(GameObject object) {
+		int size = object.mValues.size();
+		for (int k = 0; k < size; k++)
+			engine.put("p"+(k+1), object.mValues.get(k));
+		try {
+			Number r = (Number) engine.eval(objectColor[0]);
+			Number g = (Number) engine.eval(objectColor[1]);
+			Number b = (Number) engine.eval(objectColor[2]);
+			object.color[0] = (int) Math.min(Math.max(256 * r.doubleValue(), 0), 255);
+			object.color[1] = (int) Math.min(Math.max(256 * g.doubleValue(), 0), 255);
+			object.color[2] = (int) Math.min(Math.max(256 * b.doubleValue(), 0), 255);
+		} catch (ScriptException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public static void updateMode() {
+		if (removeMode) {
+			remObjects.setBackground(Color.orange);
+			selectButton.setBackground(Color.lightGray);
+			changeButton.setBackground(Color.lightGray);
+			cellButton.setBackground(Color.lightGray);
+			objectButton.setBackground(Color.yellow);
+			return;
+		}
+		remObjects.setBackground(Color.lightGray);
 		switch (clickMode) {
 			case 1:
 				selectButton.setBackground(Color.yellow);
@@ -317,6 +495,14 @@ public class MapDesigner {
 				selectButton.setBackground(Color.lightGray);
 				changeButton.setBackground(Color.lightGray);
 		}
+		if (cellMode) {
+			cellButton.setBackground(Color.orange);
+			objectButton.setBackground(Color.lightGray);
+		} else {
+			cellButton.setBackground(Color.lightGray);
+			objectButton.setBackground(Color.orange);
+		}
+		repaintFields();
 	}
 
 }
